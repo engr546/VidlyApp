94e385c39c391a175898b3cfa9afb57c5b4deaef
engr546 <engrflare@gmail.com>
2020-07-25 22:30:32 +0800

    Set Up + Exercise 2
    - Added Customer Models&Controller&View
    - Added Movies Models&Controller&View
    - Updated Bootstrap & JS dependencies

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ViewResult Index()
+        {
+            var movie = GetMovies();
+
+            return View(movie);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movies = GetMovies().SingleOrDefault(c => c.Id == id);
+
+            if (movies == null)
+                return HttpNotFound();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Wall-e" },
+                new Movie { Id = 2, Name = "Shrek" }
+            };
+        }
+
+    }
+}
