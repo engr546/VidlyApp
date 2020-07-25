d3598fbe1cd0ad57b2a1a633c227ce93af09c48e
engr546 <engrflare@gmail.com>
2020-07-25 23:16:55 +0800

    Seeding and Overriding

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,13 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required] //Data Annotation/Attribute
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribeToNewsLetter { get; set; }
+        // Navigation Property
+        public MembershipType MembershipType { get; set; }
+        // Foreign Key
+        public byte MembershipTypeId { get; set; }
     }
 }
