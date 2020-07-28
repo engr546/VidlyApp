9a332cd04808a9896498598a791828c7b7c0d9d3
engr546 <engrflare@gmail.com>
2020-07-28 20:17:57 +0800

    Validation Summary

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -38,6 +38,7 @@ namespace Vidly.Controllers
 
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
