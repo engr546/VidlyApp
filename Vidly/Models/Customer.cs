54f0b1af55ca03a75b137b2b2eb4930cbdc9f79d
engr546 <engrflare@gmail.com>
2020-07-28 20:04:04 +0800

    Custom Validations

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,13 +10,14 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required] //Data Annotation/Attribute
+        [Required(ErrorMessage = "Please enter Customer's Name")] //Data Annotation/Attribute
         [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribeToNewsLetter { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [LegalAgeMember]
         public DateTime? Birthdate { get; set; }
 
         // Navigation Property
