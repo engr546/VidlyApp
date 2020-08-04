ee64193b488fa44ba165d7dba60d08c3738ba097
engr546 <engrflare@gmail.com>
2020-08-04 16:25:14 +0800

    Using DataTables

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -8,8 +8,13 @@ namespace Vidly
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-3.5.1.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-3.5.1.min.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/bootbox.min.js",
+                        "~/Scripts/bootstrap-datepicker.min.js",
+                        "~/Scripts/DataTables/jquery.datatables.min.js",
+                        "~/Scripts/DataTables/datatables.bootstrap4.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,15 +24,11 @@ namespace Vidly
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/bootbox.min.js",
-                      "~/Scripts/bootstrap-datepicker.min.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
                       "~/Content/fontawesome-all.min.css",
                       "~/Content/bootstrap-datepicker.min.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap4.min.css",
                       "~/Content/site.css"));
         }
     }
